3f537c4a1cc8475e5e760729a1408f5f54247541
Chris Vande Velde <chris.vandevelde@student.ucll.be>
2021-03-30 11:59:13 +0200

    Thibault-Chris_Story11part

--- src/domain/Vorm.java
+++ src/domain/Vorm.java
@@ -1,4 +1,16 @@
 package domain;
 
+import java.awt.*;
+
 public abstract class Vorm {
+    private Color kleur;
+
+    public Color getKleur() {
+        return this.kleur;
+    }
+
+    public abstract String toString();
+    public abstract Omhullende getOmhullende();
+    public abstract Punt getMiddelPunt();
 }
+
